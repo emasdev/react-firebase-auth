41f01f98ed0d46ce5019911da1c8153ede4e384d
Emas <emas.dev@gmail.com>
2021-09-17 09:14:15 -0500

    add navigation

--- src/index.js
+++ src/index.js
@@ -6,7 +6,12 @@ import reportWebVitals from "./reportWebVitals";
 import App from "./components/App";
 import Firebase, { FirebaseContext } from "./components/Firebase";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <FirebaseContext.Provider value={new Firebase()}>
+    <App />
+  </FirebaseContext.Provider>,
+  document.getElementById("root")
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
