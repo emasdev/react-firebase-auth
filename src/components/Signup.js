856c3ae76d73f4f47ffc75b810994e47b32f8fa6
Emas <emas.dev@gmail.com>
2021-09-22 22:16:05 -0500

    Fix bugs

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,38 +1,28 @@
-import React, { useState } from 'react';
-import {
-  Form,
-  Input,
-  Button
-} from 'antd';
-import { withRouter } from "react-router"
-import Error from './Error';
+import React, { useState } from "react";
+import { Form, Input, Button } from "antd";
+import { withRouter } from "react-router";
+import Error from "./Error";
 
 import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 import { doc, updateDoc } from "firebase/firestore";
 
-
 const Signup = ({ setsignup, history }) => {
   const [form] = Form.useForm();
-  const [error, seterror] = useState('')
+  const [error, seterror] = useState("");
 
   const onFinish = (values) => {
-    console.log('Received values of form: ', values);
+    console.log("Received values of form: ", values);
     const auth = getAuth();
     createUserWithEmailAndPassword(auth, values.email, values.password)
       .then((userCredential) => {
         // Signed in
         //const user = userCredential.user;
-        //console.log('signed in');        // ...
-        const docRef = await setDoc(collection(db, "users"), {
-          email: values.email,
-          nombre: values.nombre,
-          apellidos: values.apellidos,
-        });
+        console.log("signed in"); // ...
       })
       .catch((error) => {
         //const errorCode = error.code;
         //const errorMessage = error.message;
-        seterror(error.message)
+        seterror(error.message);
       });
   };
 
@@ -53,9 +43,6 @@ const Signup = ({ setsignup, history }) => {
     </Form.Item>
   );
 
-
-
-
   return (
     <>
       <Form
@@ -68,18 +55,22 @@ const Signup = ({ setsignup, history }) => {
         <Form.Item>
           <h3>Registre su cuenta de IDM</h3>
         </Form.Item>
-        {error ? <Form.Item><Error mensaje={error} /></Form.Item> : null}
+        {error ? (
+          <Form.Item>
+            <Error mensaje={error} />
+          </Form.Item>
+        ) : null}
         <Form.Item
           name="email"
           label="E-mail"
           rules={[
             {
-              type: 'email',
-              message: 'The input is not valid E-mail!',
+              type: "email",
+              message: "The input is not valid E-mail!",
             },
             {
               required: true,
-              message: 'Please input your E-mail!',
+              message: "Please input your E-mail!",
             },
           ]}
         >
@@ -92,7 +83,7 @@ const Signup = ({ setsignup, history }) => {
           rules={[
             {
               required: true,
-              message: 'Please input your password!',
+              message: "Please input your password!",
             },
           ]}
           hasFeedback
@@ -103,20 +94,22 @@ const Signup = ({ setsignup, history }) => {
         <Form.Item
           name="confirm"
           label="Confirm Password"
-          dependencies={['password']}
+          dependencies={["password"]}
           hasFeedback
           rules={[
             {
               required: true,
-              message: 'Please confirm your password!',
+              message: "Please confirm your password!",
             },
             ({ getFieldValue }) => ({
               validator(_, value) {
-                if (!value || getFieldValue('password') === value) {
+                if (!value || getFieldValue("password") === value) {
                   return Promise.resolve();
                 }
 
-                return Promise.reject(new Error('The two passwords that you entered do not match!'));
+                return Promise.reject(
+                  new Error("The two passwords that you entered do not match!")
+                );
               },
             }),
           ]}
@@ -130,7 +123,7 @@ const Signup = ({ setsignup, history }) => {
           rules={[
             {
               required: true,
-              message: 'Este campo es obligatório.',
+              message: "Este campo es obligatório.",
             },
           ]}
         >
@@ -143,7 +136,7 @@ const Signup = ({ setsignup, history }) => {
           rules={[
             {
               required: true,
-              message: 'Este campo es obligatório.',
+              message: "Este campo es obligatório.",
             },
           ]}
         >
@@ -156,14 +149,14 @@ const Signup = ({ setsignup, history }) => {
           rules={[
             {
               required: true,
-              message: 'Please input your phone number!',
+              message: "Please input your phone number!",
             },
           ]}
         >
           <Input
             addonBefore={prefixSelector}
             style={{
-              width: '100%',
+              width: "100%",
             }}
           />
         </Form.Item>
@@ -179,7 +172,6 @@ const Signup = ({ setsignup, history }) => {
         </Form.Item>
       </Form>
     </>
-
   );
 };
 
