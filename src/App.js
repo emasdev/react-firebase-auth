509738dc02bb2999307d66a70e1f4b3996dcdb8c
Emas <emas.dev@gmail.com>
2021-09-24 21:51:12 -0500

    add boiler plate

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
-import React from 'react';
-
-import Layout from './components/Layout/Layout';
-import { BrowserRouter as Router } from 'react-router-dom';
-
+import React from "react";
 
 function App() {
   return (
-    <Router>
-      <Layout>Hello World</Layout>
-    </Router>
-
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,20 +1,9 @@
-import { ColorModeScript } from '@chakra-ui/react';
-import React, { StrictMode } from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import { ChakraProvider, theme } from '@chakra-ui/react'
-
-import { AuthProvider } from "./hooks/useAuth";
+import React from "react";
+import ReactDOM from "react-dom";
 
 ReactDOM.render(
-  <StrictMode>
-    <ColorModeScript />
-    <ChakraProvider theme={theme}>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </ChakraProvider>
-  </StrictMode>,
-  document.getElementById('root')
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
 );
-
